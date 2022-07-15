bdeb228133d38fb187187435710e5a2da9a2d0e5
Ch0pper26 <91217238+Ch0pper26@users.noreply.github.com>
2022-07-15 13:43:43 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../ashes.h"
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
